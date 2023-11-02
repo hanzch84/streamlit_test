c9f889c2e40120aa8579a94ee0aff4fc30090db3
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2023-11-03 04:10:51 +0900

    .

--- pages/calc.py
+++ pages/calc.py
@@ -90,7 +90,7 @@ def add_item():
 
 # 물품추가 버튼에 콜백 함수 연결
 with col_left:
-    if st.button("+ 물품추가", on_click=add_item):
+    if st.button("물품추가", on_click=add_item):
         pass
 
 quantity = 0
@@ -101,17 +101,19 @@ with col_right:
         # 계산 결과를 구합니다.
         quantity = calculate_budget(budget, item_names, item_prices)
 
-        # 결과를 화면에 표시합니다.
-        df = pd.DataFrame({
-            "품목": item_names,
-            "단가": item_prices,
-            "수량": quantity
-        })
+    # 결과를 화면에 표시합니다.
+    df = pd.DataFrame({
+        "품목": item_names,
+        "단가": item_prices,
+        "수량": quantity
+    })
+
 
 
     with col_aux:
         # 파일로 저장하는 기능을 제공합니다.
         if st.checkbox("파일로 저장"):
             df.to_csv("output.csv")
-
     st.table(df)  
+
+st.write("감사합니다.")
