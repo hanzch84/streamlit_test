cf7c5230aa2d57405a65236a0f22fac378836c35
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2023-11-03 00:18:04 +0900

    mov

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.subheader("Hello, world!")
 # #이미지 비디오 불러오기
 # st.image("https://img.freepik.com/free-vector/flat-design-mountain-landscape_23-2149172160.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697760000&semt=ais",width=700 )
 # st.video("https://www.youtube.com/watch?v=JUjpvu3mGGY", start_time=100)
-st.video("https://www.linkedin.com/feed/update/urn:li:activity:7095012114515312640?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7095012114515312640%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=8d0kspL9RJ69QVa3grJFKg%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BjnvIgj%2BsT%2BCA6UOkb%2F9VPg%3D%3D")
+st.video(r"https://www.linkedin.com/feed/update/urn:li:activity:7095012114515312640?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7095012114515312640%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=8d0kspL9RJ69QVa3grJFKg%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BjnvIgj%2BsT%2BCA6UOkb%2F9VPg%3D%3D")
 
 # st.divider()
 
