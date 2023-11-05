390dcc96e5945302cdf9b7613da067dff25af602
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2023-11-05 22:21:28 +0900

    map added

--- pages/interactive widget.py
+++ pages/interactive widget.py
@@ -13,9 +13,13 @@ st.write(f"당신은 {radio_option}을(를) 선택하셨습니다.")
 options = st.multiselect("어떤 색상을 좋아하나요?", ["Red", "Green", "Blue"])
 st.write(f"당신이 선택한 색상: {', '.join(options)}")
 
+#맵 위젯
+st.map(latitude="128.1590828",longitude="36.4109466",zoom=20)
+
 # 프로그레스 바
 'start your progress'
 my_bar = st.progress(0)
 for percent_complete in range(100):
     time.sleep(0.01)  # 0.01초마다 프로그레스 바가 업데이트됩니다.
     my_bar.progress(percent_complete + 1)
+
