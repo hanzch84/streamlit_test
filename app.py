88c2375f78962425cfe1e63b0b287a09f4a32fb0
hanzch84 <hanzch84@gmail.com>
2023-12-08 07:05:32 +0900

    wide layer test

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
+출처: https://python-programming-diary.tistory.com/137 [웹디자인 그리고, 클라우드 기반 인공지능 개발과 DevOps 실무:티스토리]
 st.title("현수의 페이지!")
 st.subheader("Hello, world!")
 
-
+col1, col2, col3 = st.columns(3)
 
 # st.caption("the page for simbudget")
 # name=st.text_input("이름을 입력해주세요.")
@@ -20,11 +22,15 @@ st.subheader("Hello, world!")
 # ##########################################
 # #이미지 비디오 불러오기
 
-st.image("a.jpg")
+col1.image("a.jpg")
+col2.image("https://photos.app.goo.gl/nkAxAsjMKgSB1ZR18")
+col3.image("https://photos.app.goo.gl/67TNaYBqtQz1pz95A")
 # st.image("https://img.freepik.com/free-vector/flat-design-mountain-landscape_23-2149172160.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697760000&semt=ais",width=700 )
 # st.video("https://www.youtube.com/watch?v=JUjpvu3mGGY", start_time=100)
-st.video("https://youtu.be/qPbffXgERvM")
+col_l, col_r = st.columns(2)
 
+col_l.video("https://youtu.be/qPbffXgERvM")
+col_r.video("https://photos.app.goo.gl/gXKYGsyjnK9RdFcs9")
 # st.divider()
 
 # st.title("Interactive Widget")
