4c7ca3c0c26dc72ddd96c91d0ac7a1ceb5096e28
hanzch84 <hanzch84@gmail.com>
2023-12-10 11:44:55 +0900

    fix usage

--- pages/calligraphy_file_handler.py
+++ pages/calligraphy_file_handler.py
@@ -347,6 +347,9 @@ def draw_grid_on_image(image, horizontal_lines, vertical_lines):
 
     return image
 
+def on_chaange_text():
+    st.session_state['first_load'] = False  # 표시되지 않도록 설정합니다.
+
 
 # 세션 상태에 'first_load' 키가 없으면 True를 설정합니다. (처음 로딩 시)
 if 'first_load' not in st.session_state:
@@ -359,8 +362,7 @@ st.write("학습지의 앞면은 따라쓰기, 뒷면은 빈 칸입니다. 뒷�
 
 # 첫 로딩 시에만 텍스트 영역에 기본값을 표시합니다.
 if st.session_state['first_load']:
-    text_to_insert = st.text_area("Enter text (13x7 characters):", height=100, value='오늘도 또 우리 수탉이 막 쫓기었다. 내가 점심을 먹고 나무를 하러 갈 양으로 나올 때이었다. 산으로 올라서려니까 등뒤에서 푸드득 푸드득 하고 닭의 횃소리가 야단이다.')
-    st.session_state['first_load'] = False  # 표시되지 않도록 설정합니다.
+    text_to_insert = st.text_area("Enter text (13x7 characters):", height=100,on_change=on_chaange_text, value='오늘도 또 우리 수탉이 막 쫓기었다. 내가 점심을 먹고 나무를 하러 갈 양으로 나올 때이었다. 산으로 올라서려니까 등뒤에서 푸드득 푸드득 하고 닭의 횃소리가 야단이다.')
 else:
     text_to_insert = st.text_area("Enter text (13x7 characters):", height=100)
 
