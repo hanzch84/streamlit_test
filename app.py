d7e83a8e43d3e7ebde100430698d095382510a8b
hanzch84 <hanzch84@gmail.com>
2023-12-18 22:52:11 +0900

    media update

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ col1.image("a.jpg")
 col2.image("https://media.licdn.com/dms/image/D5622AQGuxS8pMNND_g/feedshare-shrink_2048_1536/0/1701987220010?e=1704931200&v=beta&t=BI3n2YvwzsdHYCEfqErSi_xMiFhCnngLBa5FqMpxSkM")
 col2.image("https://media.licdn.com/dms/image/D5622AQGatP51eua6hw/feedshare-shrink_1280/0/1701987219235?e=1704931200&v=beta&t=EQLltFXtkez6WgGpbwoKekdbNZJ0e6QZh4B_Pztn6q4")
 col3.image("pi5.jpg")
+col3.image("ard1.jpg")
+
 # st.image("https://img.freepik.com/free-vector/flat-design-mountain-landscape_23-2149172160.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697760000&semt=ais",width=700 )
 # st.video("https://www.youtube.com/watch?v=JUjpvu3mGGY", start_time=100)
 col_l, col_r = st.columns(2)
